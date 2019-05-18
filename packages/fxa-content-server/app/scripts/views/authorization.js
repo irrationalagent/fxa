3f95e8fa9b0ca91ab60776d859e12f55a94b480d
Shane Tomlinson <stomlinson@mozilla.com>
2019-05-18 11:32:04 +0100

    feat(oidc): Basic support for OIDC prompt=none
    Laods of caveats:
    * Only works for trusted relying parties that do not request keys
    * Only works if the user is signed in
    * Makes no effort at "single log out"
    * Does not use "id_token_hint"
    
    fixes #640

--- packages/fxa-content-server/app/scripts/views/authorization.js
+++ packages/fxa-content-server/app/scripts/views/authorization.js
@@ -6,9 +6,30 @@
  * OAuth authorization view, redirects based on requested OAuth actions.
  */
 import BaseView from './base';
+import Cocktail from 'cocktail';
+import { OAUTH_PROMPT_NONE } from '../lib/constants';
+import CachedCredentialsMixin from './mixins/cached-credentials-mixin';
 
 class AuthorizationView extends BaseView {
   beforeRender () {
+    if (this.relier.get('prompt') === OAUTH_PROMPT_NONE) {
+      // TODO make these real errors
+      if (! this.relier.isTrusted()) {
+        throw new Error('No way, this client is not trusted');
+      }
+
+      if (this.relier.wantsKeys()) {
+        throw new Error('Cannot use prompt=none while requesting keys');
+      }
+
+      const account = this.getSignedInAccount();
+      if (account.isDefault()) {
+        throw new Error('Not signed in');
+      }
+
+      return this.useLoggedInAccount(account);
+    }
+
     const action = this.relier.get('action');
     if (action) {
       const pathname = action === 'email' ? '/oauth/' : action;
@@ -20,5 +41,10 @@ class AuthorizationView extends BaseView {
     }
   }
 }
+
+Cocktail.mixin(
+  AuthorizationView,
+  CachedCredentialsMixin,
+);
 
 export default AuthorizationView;
